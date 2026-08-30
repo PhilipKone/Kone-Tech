64fab7ae4e85235d57c1c02e9c35ddc4815648d6
Kone <philipkone45@.com>
2026-08-30 18:06:11 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'kone-tech-cache-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './favicon.svg',
+  './favicon-48x48.png',
+  './apple-touch-icon.png',
+  './favicon-192x192.png',
+  './logo512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (STATIC_ASSETS.some(a => event.request.url.includes(a.replace('./', '')))) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
